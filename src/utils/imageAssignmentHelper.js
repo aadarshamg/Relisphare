18350209ed6e15aeec8486ebda385767234512ff
Adarsh Kumar <connectfalqon@gmail.com>
2026-06-08 16:51:52 +0530

    Match product images by item type

--- src/utils/imageAssignmentHelper.js
+++ src/utils/imageAssignmentHelper.js
@@ -56,25 +56,159 @@ export const CATEGORY_IMAGES = {
   ]
 };
 
+export const PRODUCT_TYPE_IMAGES = {
+  'Oak Desk': [
+    'https://images.unsplash.com/photo-1518455027359-f3f8164ba6bd',
+    'https://images.unsplash.com/photo-1518455027359-f3f8164ba6bd'
+  ],
+  'Leather Sofa': [
+    'https://images.unsplash.com/photo-1555041469-a586c61ea9bc',
+    'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'
+  ],
+  'Dining Table': [
+    'https://images.unsplash.com/photo-1616486338812-3dadae4b4ace',
+    'https://images.unsplash.com/photo-1600210491892-03d54c0aaf87'
+  ],
+  Armchair: [
+    'https://images.unsplash.com/photo-1567538096630-e0c55bd6374c',
+    'https://images.unsplash.com/photo-1519947486511-46149fa0a254'
+  ],
+  Bookshelf: [
+    'https://images.unsplash.com/photo-1521587760476-6c12a4b040da',
+    'https://images.unsplash.com/photo-1594620302200-9a762244a156'
+  ],
+  'Coffee Table': [
+    'https://images.unsplash.com/photo-1533090481720-856c6e3c1fdc',
+    'https://images.unsplash.com/photo-1616486029423-aaa4789e8c9a'
+  ],
+  Wardrobe: [
+    'https://images.unsplash.com/photo-1595428774223-ef52624120d2',
+    'https://images.unsplash.com/photo-1618220179428-22790b461013'
+  ],
+  'Bed Frame': [
+    'https://images.unsplash.com/photo-1505693416388-ac5ce068fe85',
+    'https://images.unsplash.com/photo-1616594039964-ae9021a400a0'
+  ],
+  'Roman Coin': [
+    'https://images.unsplash.com/photo-1624862763978-f2c4f71473e4',
+    'https://images.unsplash.com/photo-1617050331773-8d79dcd0feb6'
+  ],
+  'Silver Dollar': [
+    'https://images.unsplash.com/photo-1643393670617-0915d3f6d1b8',
+    'https://images.unsplash.com/photo-1542993196-1c2a5aee4d0a'
+  ],
+  'Gold Doubloon': [
+    'https://images.unsplash.com/photo-1610375461369-d613b56474c3',
+    'https://images.unsplash.com/photo-1686599596608-6be9a4426e96'
+  ],
+  'Ancient Drachma': [
+    'https://images.unsplash.com/photo-1617050331773-8d79dcd0feb6',
+    'https://images.unsplash.com/photo-1682776839954-2b131f0d6e70'
+  ],
+  'Victorian Penny': [
+    'https://images.unsplash.com/photo-1542993196-1c2a5aee4d0a',
+    'https://images.unsplash.com/photo-1579983093077-6db76fab6477'
+  ],
+  'Abstract Canvas': [
+    'https://images.unsplash.com/photo-1541961017774-22349e4a1262',
+    'https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5'
+  ],
+  'Landscape Oil': [
+    'https://images.unsplash.com/photo-1578301978693-85fa9c0320b9',
+    'https://images.unsplash.com/photo-1579783901586-d88db74b4fe4'
+  ],
+  'Watercolor Portrait': [
+    'https://images.unsplash.com/photo-1579783900882-c0d3dad7b119',
+    'https://images.unsplash.com/photo-1580136607993-fd598cf5c4f5'
+  ],
+  'Modern Art Print': [
+    'https://images.unsplash.com/photo-1547826039-bfc35e0f1ea8',
+    'https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5'
+  ],
+  'Bronze Statue': [
+    'https://images.unsplash.com/photo-1564399579883-451a5d44ec08',
+    'https://images.unsplash.com/photo-1582555172866-f73bb12a2ab3'
+  ],
+  'Marble Bust': [
+    'https://images.unsplash.com/photo-1578301978069-45264734bcd6',
+    'https://images.unsplash.com/photo-1544967082-d9d25d867d66'
+  ],
+  'Stone Carving': [
+    'https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7',
+    'https://images.unsplash.com/photo-1590736969955-71cc94901144'
+  ],
+  'Modern Metal Sculpture': [
+    'https://images.unsplash.com/photo-1578926375605-eaf7559b1458',
+    'https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7'
+  ],
+  'Diamond Ring': [
+    'https://images.unsplash.com/photo-1605100804763-247f67b3557e',
+    'https://images.unsplash.com/photo-1515562141207-7a88fb7ce338'
+  ],
+  'Gold Necklace': [
+    'https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f',
+    'https://images.unsplash.com/photo-1611652022419-a9419f74343d'
+  ],
+  'Silver Bracelet': [
+    'https://images.unsplash.com/photo-1611591437281-460bfbe1220a',
+    'https://images.unsplash.com/photo-1617038220319-276d3cfab638'
+  ],
+  'Pearl Earrings': [
+    'https://images.unsplash.com/photo-1630019852942-f89202989a59',
+    'https://images.unsplash.com/photo-1535632787350-4e68ef0ac584'
+  ],
+  'Sapphire Pendant': [
+    'https://images.unsplash.com/photo-1617038260897-41a1f14a8ca0',
+    'https://images.unsplash.com/photo-1599643477877-530eb83abc8e'
+  ],
+  Chronograph: [
+    'https://images.unsplash.com/photo-1524592094714-0f0654e20314',
+    'https://images.unsplash.com/photo-1539874754764-5a96559165b0'
+  ],
+  'Diver Watch': [
+    'https://images.unsplash.com/photo-1548171915-e79a380a2a4b',
+    'https://images.unsplash.com/photo-1619134778706-7015533a6150'
+  ],
+  'Dress Watch': [
+    'https://images.unsplash.com/photo-1533139502658-0198f920d8e8',
+    'https://images.unsplash.com/photo-1523275335684-37898b6baf30'
+  ],
+  'Pilot Watch': [
+    'https://images.unsplash.com/photo-1434056886845-dac89ffe9b56',
+    'https://images.unsplash.com/photo-1508057198894-247b23fe5ade'
+  ],
+  'Mechanical Timepiece': [
+    'https://images.unsplash.com/photo-1509048191080-d2984bad6ae5',
+    'https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3'
+  ]
+};
+
 export const getImagesForCategory = (category) => {
   return CATEGORY_IMAGES[category] || [];
 };
 
+export const getImagesForProduct = (product) => {
+  const name = product?.name || '';
+  const matchedType = Object.keys(PRODUCT_TYPE_IMAGES).find((type) => name.includes(type));
+
+  return matchedType ? PRODUCT_TYPE_IMAGES[matchedType] : getImagesForCategory(product?.category);
+};
+
 export const assignImagesToProducts = (products) => {
-  const categoryCounters = {};
+  const imageCounters = {};
   
   return products.map(product => {
-    const category = product.category;
-    const images = getImagesForCategory(category);
+    const images = getImagesForProduct(product);
     
     if (images.length === 0) return product;
     
-    if (categoryCounters[category] === undefined) {
-      categoryCounters[category] = 0;
+    const counterKey = `${product.category || 'Uncategorized'}:${product.name || ''}`;
+    if (imageCounters[counterKey] === undefined) {
+      imageCounters[counterKey] = 0;
     }
     
-    const assignedImage = images[categoryCounters[category] % images.length];
-    categoryCounters[category]++;
+    const assignedImage = images[imageCounters[counterKey] % images.length];
+    imageCounters[counterKey]++;
     
     return {
       ...product,
